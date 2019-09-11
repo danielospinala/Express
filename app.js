86c476330e055aa6038345340c2b732b3825007d
David <54381677+DLis6@users.noreply.github.com>
2019-09-10 20:09:19 -0500

    app

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
   if(req.query.nombre){
 res.send('<h1>Hola '+req.query.nombre+'!</h1>');
 }else {
-  res.send('<h1>Hola desconocido! </h1>');
+  res.send('<h1>Hola desconocido!</h1>');
 }
 });
 
