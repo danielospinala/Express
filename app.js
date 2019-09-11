b1f8a3c1c925259d659fbc5a28e767d8c2d3cad8
David <54381677+DLis6@users.noreply.github.com>
2019-09-10 20:46:53 -0500

    App Actualizada

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/', (req, res) => {
-  if(req.query.nombre){
-res.send('<h1>Hola '+req.query.nombre+'!</h1>');
-}else {
-  res.send('<h1>Hola desconocido!</h1>');
-}
+app.get('/makers/:nombre', (req, res) => {
+const name = req.params.nombre
+const nameC = name.charAt(0).toUpperCase() + name.slice(1)
+
+res.send('<h1>Hola '+nameC+'!</h1>');
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
