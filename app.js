3ccd7f3118e1f115b63629fab21cab7ce695337f
danielospina@gmail.com <danielospina@gmail.com>
2019-09-30 19:27:24 -0500

    add Heroku2

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+const PORT = proccess.env.PORT || 3000
 
 app.get('/makers/:nombre', (req, res) => {
 const name = req.params.nombre
@@ -8,4 +9,5 @@ const nameC = name.charAt(0).toUpperCase() + name.slice(1)
 res.send('<h1>Hola '+nameC+'!</h1>');
 });
 
-app.listen(3000, () => console.log('Listening on port 3000!'));
+app.listen(PORT, () => console.log('Listening on port 3000!'))
+//app.listen(3000, () => console.log('Listening on port 3000!'));
