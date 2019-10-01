2eac99188185ed9ab69e0df650f03473a7d8b1a3
danielospina@gmail.com <danielospina@gmail.com>
2019-09-30 19:35:10 -0500

    add Heroku4

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-const PORT = proccess.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 
 app.get('/makers/:nombre', (req, res) => {
 const name = req.params.nombre
